492e477c8bf8a3737a47a570f091dda203db693f
Hollis Kwan <holliskwan.ios@gmail.com>
2022-11-19 12:34:56 -0800

    Updated

--- YouPick/LocationManager/LocationManager.swift
+++ YouPick/LocationManager/LocationManager.swift
@@ -12,7 +12,7 @@ import UIKit
 protocol LocationManagerDelegate: AnyObject {
     func didUpdateStatus(_ allowed: Bool)
 }
-
+ 
 class LocationManager: NSObject, CLLocationManagerDelegate {
     
     static let shared = LocationManager()
